e8223fad5877c435454a9f127723b51490e42963
Ali KOÇAN <alikcn35@gmail.com>
2023-02-20 08:28:23 +0300

    2202023828

--- src/main/java/com/hb07/bi_onetomany/RunnerFetch07.java
+++ src/main/java/com/hb07/bi_onetomany/RunnerFetch07.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,23 +18,52 @@ public class RunnerFetch07 {
         Session session=sf.openSession();
         Transaction tx=session.beginTransaction();
 
-        Student07 student= session.get(Student07.class,1001);
-        System.out.println(student.getBookList());
-        student.getBookList().forEach(t-> System.out.println(t));
-        student.getBookList().forEach(System.out::println);
+//        Student07 student= session.get(Student07.class,1001);
+//        System.out.println(student.getBookList());
+//        student.getBookList().forEach(t-> System.out.println(t));
+//        student.getBookList().forEach(System.out::println);
+//
+//        // !!! sql ile ogrenci bilgilerini alalim ( ogrenci ismi ve kitap ismi )
+//        String sqlQuery="select s.student_name,b.name from student07 s inner join book07 b on s.id=b.student_id";
+//        List<Object[]> resultList1= session.createSQLQuery(sqlQuery).getResultList();
+//        for (Object[] object: resultList1){
+//            System.out.println(Arrays.toString(object));
+//        }
+//
+//        // !!! hql ile ogrenci bilgilerini alalim
+//        String hqlQuery="select s.name,b.name from Student07 s inner join fetch Book07 b on s.id=b.student.id";
+//        List<Object[]> resultList2= session.createQuery(hqlQuery).getResultList();
+//        resultList2.forEach(t-> System.out.println(Arrays.toString(t)));
+//
+//        // !!! sql ile delete islemi
+//        String sqlQuery2="delete from book07";
+//        int numOfDeletedRecords=session.createSQLQuery(sqlQuery2).executeUpdate();
+//        System.out.println("silinen kayit sayisi: "+numOfDeletedRecords);
+//
+//        // !!! hql ile delete islemi
+//        String hqlQuery2="delete from Student07";
+//        int numOfDeletedRecords2= session.createQuery(hqlQuery2).executeUpdate();
+//        System.out.println("silinen kayit sayisi: "+numOfDeletedRecords2);
+//
+//        // !!! hql ile kitap ismi A Book olan kitabi siliniz
+//        String hqlQuery3="delete from Book07 b where b.name='A Book'";
+//        int numOfDeletedRecords3= session.createQuery(hqlQuery3).executeUpdate();
+//        System.out.println("silinen kayit sayisi: "+numOfDeletedRecords3);
+//
+//        // !!! 1001 id li student objemi delete methoduyla silelim
+//        Student07 student2=session.get(Student07.class,1001);
+//        session.delete(student2);
 
-        // !!! sql ile ogrenci bilgilerini alalim ( ogrenci ismi ve kitap ismi )
-        String sqlQuery="select s.student_name,b.name from student07 s inner join book07 b on s.id=b.student_id";
-        List<Object[]> resultList1= session.createSQLQuery(sqlQuery).getResultList();
-        for (Object[] object: resultList1){
-            System.out.println(Arrays.toString(object));
-        }
+        // student2.getBookList().set(0,null); // listedeki 0. indexi komple siler
 
-        // !!! hql ile ogrenci bilgilerini alalim
-        String hqlQuery="select s.name,b.name from Student07 s inner join fetch Book07 b on s.id=b.student.id";
-        List<Object[]> resultList2= session.createQuery(hqlQuery).getResultList();
-        resultList2.forEach(t-> System.out.println(Arrays.toString(t)));
+        // !!! book classinda isminin icinde Java gecen student kayitlarini alalim
+        String sqlQuery4="select * from book07 b where b.name like '%Java%'";
+        List<Object[]> resultList4= session.createSQLQuery(sqlQuery4).getResultList();
+        resultList4.forEach(t-> System.out.println(Arrays.toString(t)));
 
+        String hqlQuery5="select s from Student07 s join s.bookList b where b.name like '%Java%'";
+        List<Student07> resultList5= session.createQuery(hqlQuery5,Student07.class).getResultList();
+        resultList5.forEach(System.out::println);
 
         tx.commit();
         session.close();
--- src/main/java/com/hb08/manytomany/Student08.java
+++ src/main/java/com/hb08/manytomany/Student08.java
@@ -1,11 +1,11 @@
-package com.hb07.bi_onetomany;
+package com.hb08.manytomany;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Student07 {
+public class Student08 {
 
     @Id
     private int id;
@@ -15,8 +15,13 @@ public class Student07 {
 
     private int grade;
 
-    @OneToMany(mappedBy = "student",orphanRemoval = true,cascade = CascadeType.REMOVE)
-    private List<Book07> bookList=new ArrayList<>();
+    @ManyToMany
+    @JoinTable(
+            name = "student08_book08",
+            joinColumns = {@JoinColumn(name = "std_id")},
+            inverseJoinColumns = {@JoinColumn(name = "book_id")}
+    )
+    private List<Book08> bookList=new ArrayList<>();
 
     public int getId() {
         return id;
@@ -42,17 +47,17 @@ public class Student07 {
         this.grade = grade;
     }
 
-    public List<Book07> getBookList() {
+    public List<Book08> getBookList() {
         return bookList;
     }
 
-    public void setBookList(List<Book07> bookList) {
+    public void setBookList(List<Book08> bookList) {
         this.bookList = bookList;
     }
 
     @Override
     public String toString() {
-        return "Student07{" +
+        return "Student08{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", grade=" + grade +
