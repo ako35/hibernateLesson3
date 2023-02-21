bf1830646d90ac2fd431f603044091d76aea1683
Ali KOÇAN <alikcn35@gmail.com>
2023-02-21 15:31:16 +0300

    2212023331

---
+++ src/main/java/com/hb10/idgeneration/Student10.java
@@ -0,0 +1,41 @@
+package com.hb10.idgeneration;
+
+import javax.persistence.*;
+
+@Entity
+public class Student10 {
+    
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "sequence",sequenceName = "student_seq",initialValue = 1000,allocationSize = 10)
+    @Id
+    private int id;
+
+    @Column(name = "student_name",nullable = false)
+    private String name;
+
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
